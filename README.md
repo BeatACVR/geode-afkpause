5350c35fde7a1007f19cd31025a3115897879bf2
Beat <75857504+BeatACVR@users.noreply.github.com>
2025-10-15 17:15:40 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# AFK Pause
+A Geode mod that automatically pauses the current level when you die too many times without trying.
+
+Inspired by [this video](https://youtu.be/qQqkLQ6wF48) by GD Colon.
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+int afkDeaths = 1;
+
+class $modify(afkPlayerObject, PlayerObject) {
+	bool pushButton(PlayerButton p0) {
+		if (!PlayerObject::pushButton(p0) || !GameManager::sharedState()->getPlayLayer()) return false;
+		afkDeaths = 1;
+		return true;
+	}
+};
+
+class $modify(afkPlayLayer, PlayLayer) {
+	void resetLevel() {
+		PlayLayer::resetLevel();
+
+		if (afkDeaths >= Mod::get()->getSettingValue<int64_t>("attempt-threshold")) {
+			afkDeaths = 1;
+			this->pauseGame(false);
+		} else {
+			afkDeaths++;
+		}
+		return;
+	}
+};
